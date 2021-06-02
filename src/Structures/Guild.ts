1ae611fee5b558c6055dcca07b6575969b0d5e79
Snowflake107 <devsnowflake107@gmail.com>
2021-06-02 20:36:53 +0545

    implement basic caching

--- src/Structures/Guild.ts
+++ src/Structures/Guild.ts
@@ -1,15 +1,23 @@
 import { Structures } from "discord.js";
 import { Weeknd } from "../Base/Weeknd";
+import { CacheStorage } from "../Database/CacheStorage";
 
 Structures.extend("Guild", (Extender) => {
     return class extends Extender {
+        store: CacheStorage<any>;
+
         constructor(client: Weeknd, data: any) {
             super(client, data);
+
+            this.store = new CacheStorage();
         }
 
-        async prefix(): Promise<string> {
+        async prefix(force: boolean = false): Promise<string> {
+            if (!force && typeof this.store.get(this.id)?.prefix === "string") return this.store.get(this.id)?.prefix as string;
             const data = await (this.client as Weeknd).database.models.get("Guild")?.findOne({ id: this.id });
-            return data?.prefix ?? (this.client as Weeknd).config.DEFAULT_PREFIX;
+            const prefix = data?.prefix ?? (this.client as Weeknd).config.DEFAULT_PREFIX;
+            this.store.set(this.id, data);
+            return prefix;
         }
 
         async register() {
