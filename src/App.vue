e4cd85ebcc1c636b31b551e6847d1e9c2509c2dd
ZYROUGE <zyrouge@hotmail.com>
2021-06-07 11:49:54 +0530

    feat: add base

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>SoonTM</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
