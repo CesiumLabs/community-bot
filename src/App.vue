1c07866de0940ecb862c82b6c92574d55b560f62
ZYROUGE <zyrouge@hotmail.com>
2021-06-07 12:07:16 +0530

    refactor: add logo, color, name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        <p>SoonTM</p>
+    <div class="w-full bg-gray-900" style="height: 100vh">
+        <p
+            class="
+                w-full
+                absolute
+                text-center text-5xl
+                font-bold
+                text-blurple-500
+            "
+            style="margin-top: 40vh"
+        >
+            SoonTM
+        </p>
     </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    purge: [],
+    purge: ["./index.html", "./src/**/*{css,js,vue,ts}"],
     darkMode: false,
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: colors.trueGray,
+                blurple: {
+                    DEFAULT: "#7289DA",
+                    50: "#FFFFFF",
+                    100: "#FFFFFF",
+                    200: "#EBEEFA",
+                    300: "#C3CDEF",
+                    400: "#9AABE5",
+                    500: "#7289DA",
+                    600: "#4A67CF",
+                    700: "#304EB6",
+                    800: "#253C8E",
+                    900: "#1B2B65",
+                },
+            },
+            fontFamily: {
+                sans: "Roboto, sans-serif",
+            },
+        },
     },
     variants: {
         extend: {},
