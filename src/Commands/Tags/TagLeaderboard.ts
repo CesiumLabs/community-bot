7cbabd313dab13c97a906518bc84fdb0f17ded43
Snowflake107 <devsnowflake107@gmail.com>
2021-06-19 12:51:33 +0545

    view tags of a specific user

--- src/Commands/Tags/TagLeaderboard.ts
+++ src/Commands/Tags/TagLeaderboard.ts
@@ -17,9 +17,10 @@ class TagLeaderboard extends CommandDispatcher {
         });
     }
 
-    async execute(message: Message) {
+    async execute(message: Message, args: string[]) {
+        const user = args.length ? this.client.utils.resolveUser(args.join(" ")) : null;
         const tagdb = this.client.database.models.get("Tags")!;
-        const tags = await tagdb.find({ guild: message.guild!.id });
+        const tags = await (user ? tagdb.find({ guild: message.guild!.id, author: user.id }) : tagdb.find({ guild: message.guild!.id }));
         if (!tags || !tags.length) return message.reply("❌ | Leaderboard is not available!");
 
         const pages: MessageEmbed[] = [];
@@ -35,13 +36,14 @@ class TagLeaderboard extends CommandDispatcher {
             );
         });
 
-        if (pages.length > 1)
+        if (pages.length > 1) {
             return this.client.utils.paginateEmbed({
                 pages,
                 filter: (_, user) => user.id === message.author!.id,
                 timeout: 30000,
                 channel: message.channel
             });
+        }
 
         return message.channel.send(pages[0].setFooter(`Page ${pages.length} of ${pages.length}`));
     }
