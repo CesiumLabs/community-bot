f2f9224f5cc0e8e7a1653b61151fdaa4aacda339
Snowflake107 <devsnowflake107@gmail.com>
2021-06-19 12:51:38 +0545

    prettier

--- src/Events/Discord/interaction.ts
+++ src/Events/Discord/interaction.ts
@@ -2,7 +2,9 @@ import { EventDispatcher } from "../../Base/EventDispatcher";
 import { Interaction } from "discord.js";
 
 class InteractionEvent extends EventDispatcher {
-    execute(interaction: Interaction) { /* no-op for now */ }
+    execute(interaction: Interaction) {
+        /* no-op for now */
+    }
 }
 
 export default InteractionEvent;
