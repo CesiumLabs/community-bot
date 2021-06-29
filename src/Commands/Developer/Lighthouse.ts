2f5e533bc807f8fa7e412261ef1e0d4e02147a21
abh80 <techixspotify@gmail.com>
2021-06-29 10:29:36 +0530

    prettier

--- src/Commands/Developer/Lighthouse.ts
+++ src/Commands/Developer/Lighthouse.ts
@@ -1,4 +1,5 @@
-import { Message, MessageEmbed } from "discord.js";
+import { createCanvas } from "canvas";
+import { Message, MessageAttachment, MessageEmbed } from "discord.js";
 import { CommandDispatcher } from "../../Base/CommandDispatcher";
 import { Weeknd } from "../../Base/Weeknd";
 
@@ -67,18 +68,10 @@ class LighthouseCommand extends CommandDispatcher {
 
                 const score = (m: number) => (m < 50 ? "🟥" : m > 49 && m < 90 ? "🟨" : "🟩");
                 const sc = Object.values(lhdata).filter((x) => x < 49).length;
-
-                const embed = new MessageEmbed()
-                    .setAuthor("Lighthouse Audit Report", message.guild?.iconURL()!, data.lhr.finalUrl)
-                    .addField("Performance", `${score(lhdata.perf)} **${lhdata.perf}**`)
-                    .addField("Accessibility", `${score(lhdata.acc)} **${lhdata.acc}**`)
-                    .addField("Best Practices", `${score(lhdata.bp)} **${lhdata.bp}**`)
-                    .addField("SEO", `${score(lhdata.seo)} **${lhdata.seo}**`)
-                    .setFooter(`Took ${Math.floor((Date.now() - message.createdTimestamp) / 1000)} seconds`)
-                    .setColor(sc > 2 ? "RED" : sc === 0 ? "GREEN" : "YELLOW");
-
+                console.log(lhdata);
+                const attachment = this.makeCanvas(lhdata.perf, lhdata.acc, lhdata.bp, lhdata.seo);
                 message.reply({
-                    embeds: [embed]
+                    files: [{ attachment, name: "lighthouse.png" }]
                 });
             })
             .catch(() => {
@@ -101,6 +94,64 @@ class LighthouseCommand extends CommandDispatcher {
         } catch {
             return null;
         }
+    }
+    makeCanvas(perf: number, acc: number, bp: number, seo: number) {
+        const canvas = createCanvas(800, 400);
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#ffffff";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        let pfColors = this.getColors(perf);
+        this.drawCircle(100, 180, 60, (perf / 100) * 1.5, pfColors[0], pfColors[1], ctx);
+        this.writeTextInsideCircle(100, 180, perf.toString(), pfColors[0], ctx);
+
+        const accColors = this.getColors(acc);
+        this.drawCircle(300, 180, 60, (acc / 100) * 1.5, accColors[0], accColors[1], ctx);
+        this.writeTextInsideCircle(300, 180, acc.toString(), accColors[0], ctx);
+
+        const bpColors = this.getColors(bp);
+        this.drawCircle(500, 180, 60, (bp / 100) * 1.5, bpColors[0], bpColors[1], ctx);
+        this.writeTextInsideCircle(500, 180, bp.toString(), bpColors[0], ctx);
+
+        const seoColors = this.getColors(seo);
+        this.drawCircle(700, 180, 60, (seo / 100) * 1.5, seoColors[0], seoColors[1], ctx);
+        this.writeTextInsideCircle(700, 180, seo.toString(), seoColors[0], ctx);
+
+        ctx.fillStyle = "#000";
+        ctx.font = "20px Arial";
+        ctx.fillText("Performance", 43, 280);
+        ctx.fillText("Accessibility", 250, 280);
+        ctx.fillText("Best Practices", 440, 280);
+        ctx.fillText("SEO", 680, 280);
+
+        return canvas.toBuffer();
+    }
+    getColors(i: number) {
+        if (i >= 0 && i <= 49) return ["red", "#fad7d7"];
+        else if (i >= 50 && i <= 89) return ["#fb8c00", "#ffe8cc"];
+        else if (i >= 90) return ["#18b663", "#d1f1e0"];
+        return ["red", "#fad7d7"];
+    }
+    drawCircle(x: number, y: number, rad: number, eAngle: number, borderColor: string, lightBorderColor: string, ctx: CanvasRenderingContext2D) {
+        ctx.beginPath();
+        ctx.arc(x, y, rad, 0, 2 * Math.PI);
+        ctx.lineWidth = 8;
+        ctx.fillStyle = lightBorderColor;
+        ctx.strokeStyle = lightBorderColor;
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.arc(x, y, rad, -(Math.PI / 2), eAngle * Math.PI);
+        ctx.strokeStyle = borderColor;
+        ctx.stroke();
+        ctx.closePath();
+    }
+    writeTextInsideCircle(x: number, y: number, text: string, color: string, ctx: CanvasRenderingContext2D) {
+        ctx.font = "bold 30px sans-serif";
+        ctx.fillStyle = color;
+        if (text.length == 3) return ctx.fillText(text, x - 28, y + 12);
+        ctx.fillText(text, x - 18, y + 12);
     }
 }
 
